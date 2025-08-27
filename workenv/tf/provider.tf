c57f685ee173e5a54de6710f9ecbb500e060b925
Tim <code@tim.life>
2025-08-26 17:22:28 -0700

    🔼⚙️ [skip ci] auto-commit

---
+++ workenv/tf/provider.tf
@@ -0,0 +1,15 @@
+
+
+terraform {
+  required_providers {
+    pyvider = {
+      source = "provide-io/pyvider"
+      version = "0.0.2"
+    }
+  }
+}
+
+provider "pyvider" {
+  # Configuration options
+}
+
