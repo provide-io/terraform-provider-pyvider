310ed1225684f570b6a47f039abc93dbde713167
Tim <code@tim.life>
2025-09-13 13:47:39 -0700

    🔼⚙️ [skip ci] auto-commit

--- examples/functions-demo/main.tf
+++ examples/functions-demo/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     pyvider = {
       source  = "registry.terraform.io/provide-io/pyvider"
-      version = "~> 0.0.3"
+      version = "~> 0.0.5"
     }
   }
 }
